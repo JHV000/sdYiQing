80d3f0fdf37e4836f7727be95d6025f4def1fd55
JHV000 <2226115031@qq.com>
2020-02-26 14:50:01 +0800

    新建dev供开发使用

--- src/common/axios.js
+++ src/common/axios.js
@@ -1,10 +1,10 @@
 //axios-init.js 
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8081/api/'; // 配置axios请求的地址
+axios.defaults.baseURL = 'https://youthapi.sdut.edu.cn/api'; // 配置axios请求的地址
 // axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 // axios.defaults.crossDomain = true;
-axios.defaults.withCredentials = true; //设置cross跨域 并设置访问权限 允许跨域携带cookie信息
+axios.defaults.withCredentials = false; //设置cross跨域 并设置访问权限 允许跨域携带cookie信息
 // axios.defaults.headers.common['Authorization'] = ''; // 设置请求头为 Authorization
 
 //配置发送请求前的拦截器 可以设置token信息 
